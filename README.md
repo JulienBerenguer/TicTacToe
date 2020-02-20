3bb9b2042bab9a8c89603f7e41c5022fb9fa358e
JulienBerenguer <julienberenguer26@gmail.com>
2020-02-20 12:25:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TicTacToe
+ Unreal tic-tac-toe game with ai.
---
+++ Source/TicTacToe/TicTacToeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TicTacToeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TICTACTOE_API ATicTacToeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

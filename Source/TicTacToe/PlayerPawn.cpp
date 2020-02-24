d6664f76f082f5fa294e010fb3ed18a43f1bb42d
JulienBerenguer <julienberenguer26@gmail.com>
2020-02-24 21:51:58 +0100

    Communication Widget -> PlayerPawn
    - Communication Widget -> PlayerPawn selon l'appui de quel bouton
    - Gestion de la liste du morpion (actuellement crashe)
    
    - TODO : réparer la liste
    - TODO : mettre à jour d'image du bouton selon le joueur (Communication PlayerPawn -> Widget)
    - TODO : condition de victoire et réinitialisation du niveau

--- Source/TicTacToe/PlayerPawn.cpp
+++ Source/TicTacToe/PlayerPawn.cpp
@@ -16,25 +16,30 @@ void APlayerPawn::BeginPlay()
 {
 	Super::BeginPlay();
 
-	if (wMainMenu) // Check if the Asset is assigned in the blueprint.
+	if (WidgetClass) // Check if the Asset is assigned in the blueprint.
 	{
-		FStringClassReference MyWidgetClassRef(TEXT("/Game/Blueprints/Widgets/WBP_TicTacToe"));
+		// Automatique
+		/*FStringClassReference MyWidgetClassRef(TEXT("/Game/Blueprints/Widgets/WBP_TicTacToe"));
 		if (UClass* MyWidgetClass = MyWidgetClassRef.TryLoadClass<UUserWidget>())
 		{
-			MyMainMenu = CreateWidget<UUserWidget>(PlayerController, MyWidgetClass);
+			GameWidget = CreateWidget<UUserWidget>(GetWorld(), MyWidgetClass);
 			// Do stuff with MyWidget World / GameInstance / PlayerController
-		}
+		}*/
 
 		// Create the widget and store it.
-		//MyMainMenu = CreateWidget<UUserWidget>(this, wMainMenu);
+		GameWidget = CreateWidget<UUserWidget>(GetWorld(), WidgetClass);
+
+		// L'initialise comme owner
+		GameWidget->SetOwningPlayer(GetWorld()->GetFirstPlayerController());
 
 		// now you can use the widget directly since you have a referance for it.
 		// Extra check to  make sure the pointer holds the widget.
-		if (MyMainMenu)
+		if (GameWidget)
 		{
 			//let add it to the view port
-			MyMainMenu->AddToViewport();
+			GameWidget->AddToViewport();
 		}
+		else UE_LOG(LogTemp, Warning, TEXT("ERROR PlayerPawn : couldn't load widget"));
 
 		//Show the Cursor.
 		GetWorld()->GetFirstPlayerController()->bShowMouseCursor = true;
@@ -55,3 +60,30 @@ void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 
 }
 
+
+
+
+void APlayerPawn::TicTacToeGame(FIntVector ButtonPressed) {
+	UE_LOG(LogTemp, Warning, TEXT("Button : %d / %d"), ButtonPressed.X, ButtonPressed.Y);
+
+	// Value : 0 = blank | 1 = cross | 2 = round
+	uint8 Value = 0;
+	if (!PlayerTurn) Value = 1;
+	else Value = 2;
+
+	// If blank box
+	if (BoxList[ButtonPressed.X][ButtonPressed.Y] == 0) {
+		BoxList[ButtonPressed.X][ButtonPressed.Y] = Value;
+
+		// Change turn
+		PlayerTurn = !PlayerTurn;
+	}
+	
+	UE_LOG(LogTemp, Warning, TEXT("> BoxList"));
+	for (int i = 0; i < BoxNumber; i++) {
+		for (int j = 0; j < BoxNumber; j++) {
+			UE_LOG(LogTemp, Warning, TEXT("Box [%d][%d] = [%d]"), i, j, BoxList[i][j])
+		}
+	}
+	
+}
--- Source/TicTacToe/PlayerPawn.h
+++ Source/TicTacToe/PlayerPawn.h
@@ -5,35 +5,51 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Blueprint/UserWidget.h"
+#include "Math/IntVector.h"
 #include "PlayerPawn.generated.h"
+
+#define BoxNumber 3
 
 UCLASS()
 class TICTACTOE_API APlayerPawn : public APawn
 {
 	GENERATED_BODY()
 
+	// DEFAULT CODE
 public:
 	// Sets default values for this pawn's properties
 	APlayerPawn();
-
-public:
-	// Reference UMG Asset in the Editor
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widgets")
-		TSubclassOf<class UUserWidget> wMainMenu;
-
-	// Variable to hold the widget After Creating it.
-	UUserWidget* MyMainMenu;
-
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+
+	// VARIABLES
+public:
+	// Reference UMG Asset in the Editor
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+		TSubclassOf<class UUserWidget> WidgetClass;
+
+	// Variable to hold the widget After Creating it.
+	UUserWidget* GameWidget;
+
+	// List of tic tact toe boxes (0 = blank | 1 = cross | 2 = round)
+	uint8 BoxList[BoxNumber][BoxNumber] = { 0 };
+
+	// Player turn
+	bool PlayerTurn = false;
+
+
+	// METHODS
+public:
+	UFUNCTION(BlueprintCallable)
+		void TicTacToeGame(FIntVector ButtonPressed);
 };
